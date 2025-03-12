2ab8aff5f9833281f0476090bc6816d42c1ab252
anurag.khard <anurag.khard@acko.tech>
2025-03-13 03:43:21 +0530

    constants replace fix

--- backend/src/main/java/com/acko/tool/controller/QuestionController.java
+++ backend/src/main/java/com/acko/tool/controller/QuestionController.java
@@ -10,7 +10,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import static com.acko.tool.Constants.Constants.ASSESSMENT_CONFIG;
+import static com.acko.tool.constants.Constants.ASSESSMENT_CONFIG;
+
 
 @RestController
 @RequestMapping("/questions")
