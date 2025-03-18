5c166d45ec4fcc7f077cae1394ee6ce6d88f2fdc
anurag.khard <anurag.khard@acko.tech>
2025-03-19 02:34:24 +0530

    constants issue fix

--- backend/src/main/java/com/acko/tool/constants/Constants.java
+++ backend/src/main/java/com/acko/tool/constants/Constants.java
@@ -1,6 +1,10 @@
-package com.acko.tool.Constants;
+package com.acko.tool.constants;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Constants {
-
-    public static final  String ASSESSMENT_CONFIG = "/assessment-questions/config";
+    public static final Integer MAX_AGGREGATION_BUCKET_SIZE = 200;
+    public static final String ASSESSMENT_CONFIG = "/assessment-questions/config";
 }
