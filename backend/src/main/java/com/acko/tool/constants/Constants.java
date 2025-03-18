26062080e32e6e1e1cb41f9b404e2ad436719ffa
anurag.khard <anurag.khard@acko.tech>
2025-03-19 02:30:45 +0530

    faulty commit

--- backend/src/main/java/com/acko/tool/constants/Constants.java
+++ backend/src/main/java/com/acko/tool/constants/Constants.java
@@ -1,10 +1,6 @@
-package com.acko.tool.constants;
+package com.acko.tool.Constants;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Constants {
-    public static final Integer MAX_AGGREGATION_BUCKET_SIZE = 200;
-    public static final String ASSESSMENT_CONFIG = "/assessment-questions/config";
+
+    public static final  String ASSESSMENT_CONFIG = "/assessment-questions/config";
 }
