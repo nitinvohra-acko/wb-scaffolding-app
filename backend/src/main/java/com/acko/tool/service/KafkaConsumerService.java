60722e3b2976c9269241f37aa7cbbc80d04c99ea
Triya Byadwal <triya.byadwal_int@acko.tech>
2025-04-08 09:52:47 +0530

    crm tool separation

--- backend/src/main/java/com/acko/tool/service/KafkaConsumerService.java
+++ backend/src/main/java/com/acko/tool/service/KafkaConsumerService.java
@@ -1,6 +1,5 @@
 package com.acko.tool.service;
 
-import com.acko.tool.entity.Task;
 import com.acko.tool.model.KafkaMessage;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -26,62 +25,6 @@ public class KafkaConsumerService {
 
     @Autowired
     WorkflowService workflowService;
-
-    @Autowired
-    TasksService tasksService;
-
-
-    @KafkaListener(topics = "wb-scaffolding", groupId = "my-group-1")
-    public void consumeMainWorkflowEvent(String message) {
-        try {
-            JsonNode jsonNode = objectMapper.readTree(message);
-            KafkaMessage kafkaMessage = objectMapper.treeToValue(jsonNode, KafkaMessage.class);
-            log.info("Inside external kafka consumer " + kafkaMessage.getEventId());
-            Map<String, Object> payload = new HashMap<>();
-            payload.put("event_id", kafkaMessage.getEventType());
-            Object response = ruleEngineService.execute("MainToolEventDecision", payload);
-            if (Objects.nonNull(response) && response instanceof List && !((List<?>) response).isEmpty()) {
-                Map<String, Object> responseMap = objectMapper.convertValue(((List<?>) response).get(0), new TypeReference<Map<String, Object>>() {
-                });
-                if (responseMap.get("action_type").toString().equalsIgnoreCase("bpmn")) {
-                    ruleEngineService.startBpmnProcess(responseMap.get("workflow_name").toString(), kafkaMessage.getPayload());
-                } else if (responseMap.get("action_type").toString().equalsIgnoreCase("bpmn_event")) {
-                    if (Objects.nonNull(kafkaMessage.getPayload().get("task_id"))) {
-                        String taskId = kafkaMessage.getPayload().get("task_id").toString();
-                        Task<?> task = tasksService.fetchTaskById(taskId);
-                        String workflowId = task.getWorkflowInstanceId();
-                        kafkaMessage.getPayload().put("workflow_id", workflowId);
-                    }
-                    workflowService.sendEventToWorkflow(responseMap.get("workflow_name").toString(), responseMap.get("event_name").toString(), kafkaMessage.getPayload());
-                } else log.info("No action taken");
-            }else log.info("No action taken");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @KafkaListener(topics = "wb-scaffolding", groupId = "my-group")
-    public void consumeUWToolEvent(String message) {
-        try {
-            JsonNode jsonNode = objectMapper.readTree(message);
-            KafkaMessage kafkaMessage = objectMapper.treeToValue(jsonNode, KafkaMessage.class);
-            Map<String, Object> payload = new HashMap<>();
-            log.info("Inside internal kafka consumer " + kafkaMessage.getEventId());
-            payload.put("event_id", kafkaMessage.getEventType());
-            Object response = ruleEngineService.execute("UWToolWorkbenchToolEventDecision", payload);
-            if (Objects.nonNull(response) && response instanceof List && !((List<?>) response).isEmpty()) {
-                Map<String, Object> responseMap = objectMapper.convertValue(((List<?>) response).get(0), new TypeReference<Map<String, Object>>() {
-                });
-                if (responseMap.get("action_type").toString().equalsIgnoreCase("bpmn")) {
-                    ruleEngineService.startBpmnProcess(responseMap.get("workflow_name").toString(), kafkaMessage.getPayload());
-                } else if (responseMap.get("action_type").toString().equalsIgnoreCase("bpmn_event")) {
-                    workflowService.sendEventToWorkflow(responseMap.get("workflow_name").toString(), responseMap.get("event_name").toString(), kafkaMessage.getPayload());
-                } else log.info("No action taken");
-            }else log.info("No action taken");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
     @KafkaListener(topics = "wb-scaffolding", groupId = "my-group-2")
     public void consumeCRMToolEvent(String message) {
